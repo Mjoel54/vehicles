ac81c396cc374ee487714456a2279db8d845c279
Mitch <mitchjoelklein@hotmail.com>
2024-10-27 11:44:11 +1100

    Completed first draft of the trucks module

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -65,12 +65,9 @@ class Motorbike extends Vehicle {
       console.log(`Weight: ${this.weight} lbs`);
       console.log(`Top Speed: ${this.topSpeed} mph`);
     // Print details of the wheels
-      console.log(
-        `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
-      );
-      console.log(
-        `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
-      );
+    for (let i = 0; i < this.wheels.length; i++) {
+      console.log( `Wheel: ${i + 1}: ${this.wheels[i].getDiameter} inch with a ${this.wheels[i].getTireBrand} tire`);
+    }
   }
 }
 
